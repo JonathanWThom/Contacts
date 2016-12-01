df87b85958414fad30dddb6985570cfba4b19440
Jonathan Thom and Marty Kovach <jonathan@>
2016-12-01 08:38:27 -0800

    add passing spec for company_name method.

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,9 +1,10 @@
 class Contact
-  attr_reader(:first_name, :last_name, :job_title)
+  attr_reader(:first_name, :last_name, :job_title, :company_name)
   define_method(:initialize) do |attribute|
     @first_name = attribute.fetch(:first_name)
     @last_name = attribute.fetch(:last_name)
     @job_title = attribute.fetch(:job_title)
+    @company_name = attribute.fetch(:company_name)
   end
 
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -3,7 +3,7 @@ require('contact')
 
 describe(Contact) do
   before() do
-    @test_contact = Contact.new({:first_name => "bob", :last_name => "Lablah", :job_title => "programmer"})
+    @test_contact = Contact.new({:first_name => "bob", :last_name => "Lablah", :job_title => "programmer", :company_name => "epicodus"})
   end
 
   describe("#first_name")do
@@ -21,6 +21,11 @@ describe(Contact) do
   describe("#job_title")do
     it("return the job title of the contact object") do
       expect(@test_contact.job_title()).to(eq("programmer"))
+    end
+  end
+  describe("#company_name")do
+    it("return the name of the company the object works for") do
+      expect(@test_contact.company_name()).to(eq("epicodus"))
     end
   end
 end
