59118985608f5e226cf6c9263fd1f07a350040ae
Jonathan Thom and Marty Kovach <jonathan@>
2016-12-01 11:21:17 -0800

    Add all, clear, and save methods to email class

--- lib/email.rb
+++ lib/email.rb
@@ -1,7 +1,21 @@
 class Email
+  @@emails = []
+
   attr_reader(:email_address, :email_type)
   define_method(:initialize) do |attributes|
     @email_address = attributes.fetch(:email_address)
     @email_type = attributes.fetch(:email_type)
+  end
+
+  define_singleton_method(:all) do
+    @@emails
+  end
+
+  define_method(:save) do
+    @@emails.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@emails = []
   end
 end
--- spec/email_spec.rb
+++ spec/email_spec.rb
@@ -3,6 +3,7 @@ require('email')
 
 describe(Email) do
   before do
+    Email.clear()
     @test_email = Email.new({:email_address => "zuckerberg@gmail.com", :email_type => "Work"})
   end
 
@@ -15,6 +16,27 @@ describe(Email) do
   describe('#email_type') do
     it('will return the email type') do
       expect(@test_email.email_type()).to(eq("Work"))
+    end
+  end
+
+  describe('.all') do
+    it('will be empty at first') do
+      expect(Email.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('it will save an email') do
+      @test_email.save()
+      expect(Email.all()).to(eq([@test_email]))
+    end
+  end
+
+  describe('.clear') do
+    it('it will clear out the emails') do
+      @test_email.save()
+      Email.clear()
+      expect(Email.all()).to(eq([]))
     end
   end
 end
