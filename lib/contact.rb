44142c79215ae38a4b98211775726f9698b1cf5f
Jonathan Thom and Marty Kovach <jonathan@>
2016-12-01 08:29:44 -0800

    add passing spec for last_name method.:

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,7 +1,8 @@
 class Contact
-  attr_reader (:first_name)
+  attr_reader(:first_name, :last_name)
   define_method(:initialize) do |attribute|
     @first_name = attribute.fetch(:first_name)
+    @last_name = attribute.fetch(:last_name)
   end
 
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -5,10 +5,15 @@ describe(Contact) do
 
   describe("#first_name")do
     it("return the first name of the contact object") do
-      test_contact = Contact.new({:first_name => "bob"})
+      test_contact = Contact.new({:first_name => "bob", :last_name => "Lablah"})
       expect(test_contact.first_name()).to(eq("bob"))
     end
   end
 
-  describe("#")
+  describe("#last_name")do
+    it("return the last name of the contact object") do
+      test_contact = Contact.new({:first_name => "bob", :last_name => "Lablah"})
+      expect(test_contact.last_name()).to(eq("Lablah"))
+    end
+  end
 end
