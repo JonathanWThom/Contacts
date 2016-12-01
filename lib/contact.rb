8ba26ef8090a4a0ca860341c0652d722a556f533
Jonathan Thom and Marty Kovach <jonathan@>
2016-12-01 08:34:41 -0800

    add passing spec for job_title method.

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,8 +1,9 @@
 class Contact
-  attr_reader(:first_name, :last_name)
+  attr_reader(:first_name, :last_name, :job_title)
   define_method(:initialize) do |attribute|
     @first_name = attribute.fetch(:first_name)
     @last_name = attribute.fetch(:last_name)
+    @job_title = attribute.fetch(:job_title)
   end
 
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -2,18 +2,25 @@ require('rspec')
 require('contact')
 
 describe(Contact) do
+  before() do
+    @test_contact = Contact.new({:first_name => "bob", :last_name => "Lablah", :job_title => "programmer"})
+  end
 
   describe("#first_name")do
     it("return the first name of the contact object") do
-      test_contact = Contact.new({:first_name => "bob", :last_name => "Lablah"})
-      expect(test_contact.first_name()).to(eq("bob"))
+      expect(@test_contact.first_name()).to(eq("bob"))
     end
   end
 
   describe("#last_name")do
     it("return the last name of the contact object") do
-      test_contact = Contact.new({:first_name => "bob", :last_name => "Lablah"})
-      expect(test_contact.last_name()).to(eq("Lablah"))
+      expect(@test_contact.last_name()).to(eq("Lablah"))
+    end
+  end
+
+  describe("#job_title")do
+    it("return the job title of the contact object") do
+      expect(@test_contact.job_title()).to(eq("programmer"))
     end
   end
 end
