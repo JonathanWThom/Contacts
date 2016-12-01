938835fbb98f14561fcc95be9e931f784faf8738
Jonathan Thom and Marty Kovach <jonathan@>
2016-12-01 11:08:59 -0800

    Add all, save, clear methods for phone number

--- lib/phone_number.rb
+++ lib/phone_number.rb
@@ -1,8 +1,22 @@
 class Phone_number
   attr_reader(:number, :type, :area)
+  @@phone_numbers = []
+
   define_method(:initialize) do |attributes|
     @number = attributes.fetch(:number)
     @type = attributes.fetch(:type)
     @area = attributes.fetch(:area)
+  end
+
+  define_singleton_method(:all) do
+    @@phone_numbers
+  end
+
+  define_method(:save) do
+    @@phone_numbers.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@phone_numbers = []
   end
 end
--- spec/phone_number_spec.rb
+++ spec/phone_number_spec.rb
@@ -1,25 +1,49 @@
 require('rspec')
 require('phone_number')
 
-describe(Phone_number)do
+describe(Phone_number) do
+    before() do
+      Phone_number.clear()
+      @test_number = Phone_number.new({:number => "1231234", :area => "503", :type => "home"})
+    end
+
     describe("#number") do
       it('return the digits for the phone number') do
-        test_number = Phone_number.new({:number => "1231234", :area => "503", :type => "home"})
-        expect(test_number.number()).to(eq("1231234"))
+        expect(@test_number.number()).to(eq("1231234"))
       end
     end
 
     describe("#area") do
       it('return the area code for the phone number') do
-        test_number = Phone_number.new({:number => "1231234", :area => "503", :type => "home"})
-        expect(test_number.area()).to(eq("503"))
+        expect(@test_number.area()).to(eq("503"))
       end
     end
 
     describe("#type") do
       it('return the type of phone number') do
-        test_number = Phone_number.new({:number => "1231234", :area => "503", :type => "home"})
-        expect(test_number.type()).to(eq("home"))
+        expect(@test_number.type()).to(eq("home"))
       end
     end
+
+    describe(".all") do
+      it('will be empty at first') do
+        expect(Phone_number.all()).to(eq([]))
+      end
+    end
+
+    describe("#save") do
+      it('will save a new phone number') do
+        @test_number.save()
+        expect(Phone_number.all()).to(eq([@test_number]))
+      end
+    end
+
+    describe(".clear") do
+      it('will clear all phone numbers') do
+        @test_number.save()
+        Phone_number.clear()
+        expect(Phone_number.all()).to(eq([]))
+      end
+    end
+
 end
