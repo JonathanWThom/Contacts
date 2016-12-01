6a043da5b2af9941cb7b5c961d60db9cb585960a
Jonathan Thom and Marty Kovach <marty@>
2016-12-01 10:50:42 -0800

    Add all, save, and clear methods for addresses

--- lib/address.rb
+++ lib/address.rb
@@ -1,5 +1,6 @@
 class Address
   attr_reader(:street_address, :city, :state, :zip, :type)
+  @@addresses = []
 
   define_method(:initialize) do |attributes|
     @street_address = attributes.fetch(:street_address)
@@ -8,4 +9,17 @@ class Address
     @zip = attributes.fetch(:zip)
     @type = attributes.fetch(:type)
   end
+
+  define_singleton_method(:all) do
+    @@addresses
+  end
+
+  define_method(:save) do
+    @@addresses.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@addresses = []
+  end
+
 end
--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -3,6 +3,7 @@ require('address')
 
 describe(Address) do
   before() do
+    Address.clear()
     @test_address = Address.new({:street_address => "100 NE Waffle St.", :city => 'Springfield', :state => 'Oregon', :zip => '12345', :type => 'Home'})
   end
 
@@ -33,6 +34,27 @@ describe(Address) do
   describe("#type") do
     it('return the type') do
       expect(@test_address.type()).to(eq('Home'))
+    end
+  end
+
+  describe(".all") do
+    it('will be empty at first') do
+      expect(Address.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it('will save a new address') do
+      @test_address.save()
+      expect(Address.all()).to(eq([@test_address]))
+    end
+  end
+
+  describe(".clear") do
+    it('will clear all addresses') do
+      @test_address.save()
+      Address.clear()
+      expect(Address.all()).to(eq([]))
     end
   end
 end
